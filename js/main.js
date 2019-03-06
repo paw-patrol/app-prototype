2539e0c2bb998aa6f5fd9d156d3b52f674ac1399
gwenchiasson <graphic@SJH510-03M.local>
2019-03-06 14:48:32 -0500

    fixed errors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let dogClients = [
   {
-    id: `01`
+    id: `01`,
     name: `Pippy`,
     age: `3`,
     potty: `true`,
@@ -17,10 +17,14 @@ let dogClients = [
       {feeding: {morning: {h:18, m:0}, evening: {h:18, m:30}}},
       {medicaton: null},
     ],
+    contact: {
+      phone: `647-456-9877`,
+      email: `pippy@gmail.com`
+    },
     notes: `Pippy doesn't get along with other dogs very well. Avoid encounters when possible.`,
   },
   {
-    id: `02`
+    id: `02`,
     name: `Fluffers`,
     age: `2`,
     potty: `true`,
@@ -41,10 +45,14 @@ let dogClients = [
         evening: {h:18, m:30}
       },
     ],
+    contact: {
+      phone: `647-555-6578`,
+      email: `fluffers@gmail.com`
+    },
     notes: `Make sure medication is taken with food. Enjoys belly scratches A LOT. `,
   },
   {
-    id: `03`
+    id: `03`,
     name: `Bruce`,
     age: `6`,
     potty: `true`,
@@ -61,10 +69,14 @@ let dogClients = [
       {feeding: {morning: {h:7, m:30}, evening: {h:18, m:30}}},
       {medicaton: null},
     ],
+    contact: {
+      phone: `416-906-7899`,
+      email: `bruce@gmail.com`
+    },
     notes: null,
   },
   {
-    id: `03`
+    id: `04`,
     name: `Wayne`,
     age: `3`,
     potty: `true`,
@@ -81,10 +93,14 @@ let dogClients = [
       {feeding: {morning: {h:9, m:0}, evening: {h:18, m:0}}},
       {medicaton: null},
     ],
+    contact: {
+      phone: `902-220-4635`,
+      email: `wayne@gmail.com`
+    },
     notes: `Scared of garbage cans.`,
   },
   {
-    id: `04`
+    id: `05`,
     name: `Shelly`,
     age: `4`,
     potty: `true`,
@@ -101,8 +117,16 @@ let dogClients = [
       {feeding: {morning: {h:8, m:30}, evening: {h:17, m:30}}},
       {medicaton: null},
     ],
+    contact: {
+      phone: `687-622-4123`,
+      email: `shelly@gmail.com`
+    },
     notes: `Shelly can be a little nippy at other dogs. Ensure to give her a long enough walk to exhaust some of her energy.`,
   },
 ];
 
-console.log(dogClients);
+// var clients = dogClients.map(dc => dc.name);
+
+// console.log(clients);
+
+document.getElementById('about').innerHTML = dogClients.map(dc => `<li>${dc.name}</li>`).join('');
